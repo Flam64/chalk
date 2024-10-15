fbbdc2c76278d3ccff1d93a4561c39ab9b8f6a9a
Flam64 <celio.mozes.rocha@gmail.com>
2024-10-15 11:42:26 +0200

    Feat : version finale

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+// collègue 1
+console.log(chalk.blue("Amandine"));
+console.log(chalk.red("Marco"));
+console.log(chalk.green("Benjamin"));
+console.log(chalk.yellow("Romain"));
